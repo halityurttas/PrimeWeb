4536106acd0bb1852cbe1c571d95b018f73a413c
Halit Yurttas <halityurttas@gmail.com>
2017-10-20 01:53:26 +0300

    Pointless

---
+++ PrimeWeb/PrimeWeb.Core/Logging/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PrimeWeb.Core.Logging
+{
+    public interface ILogger
+    {
+        void Add(LogLevels logLevels, string description, string detail, DateTime logDate);
+    }
+}

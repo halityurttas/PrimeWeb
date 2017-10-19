e8ab809ffa9a052d80ae9cdd3a0486731bfbd920
Halit Yurttas <halityurttas@gmail.com>
2017-10-20 01:51:50 +0300

    Remove logging

--- PrimeWeb/PrimeWeb.Core/Logging/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace PrimeWeb.Core.Logging
-{
-    public interface ILogger
-    {
-        void Add(LogLevels logLevels, string description, string detail, DateTime logDate);
-    }
-}
--- PrimeWeb/PrimeWeb.Core/Logging/LogLevels.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrimeWeb.Core.Logging
-{
-    public enum LogLevels
-    {
-        Information = 10,
-        Warning = 20,
-        Danger = 30
-    }
-}

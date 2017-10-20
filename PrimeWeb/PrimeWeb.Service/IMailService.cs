756dd0bc866eb477a8211b7c67c2f78672dc6990
Halit Yurttas <halityurttas@gmail.com>
2017-10-21 00:37:26 +0300

    Mail service continue

---
+++ PrimeWeb/PrimeWeb.Service/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Service
+{
+    public interface IMailService
+    {
+        Task<bool> SendAsync(string from, string fromName, string to, string toName, string subject, string message);
+        Task<bool> SendWithTemplateAsync(string from, string fromName, string to, string toName, string subject, string template, Dictionary<string, object> globalVars, Dictionary<string, object> vars);
+    }
+}
---
+++ PrimeWeb/PrimeWeb.Service/MailService/SmtpService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Service.MailService
+{
+    public class SmtpService: IMailService
+    {
+        private SmtpClient client;
+        private ILogService logService;
+
+        public SmtpService(ILogService logService, string host, int port, string username, string password, bool enableSSL)
+        {
+            this.logService = logService;
+            client = new SmtpClient(host, port);
+            client.Credentials = new NetworkCredential(username, password);
+            client.EnableSsl = enableSSL;
+        }
+
+        public async Task<bool> SendAsync(string from, string fromName, string to, string toName, string subject, string message)
+        {
+            var fromAddress = new MailAddress(from, fromName);
+            var toAddress = new MailAddress(to, toName);
+            var mailMessage = new MailMessage(fromAddress, toAddress);
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = message;
+            try
+            {
+                await client.SendMailAsync(mailMessage);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logService.Error("Mail error", ex);
+                return false;
+            }
+            
+        }
+
+        public Task<bool> SendWithTemplateAsync(string from, string fromName, string to, string toName, string subject, string template, Dictionary<string, object> globalVars, Dictionary<string, object> vars)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}

d7f94187cf068dfcf99433e5ff41eb54631ecf2f
Halit Yurttas <halityurttas@gmail.com>
2017-10-22 04:22:55 +0300

    Continue

---
+++ PrimeWeb/PrimeWeb.Service/MailService/MandrillMailService.cs
@@ -0,0 +1,54 @@
+﻿using Mandrill;
+using Mandrill.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Service.MailService
+{
+    public class MandrillMailService: IMailService
+    {
+        private MandrillApi api;
+        private ILogService logService;
+
+        public MandrillMailService(ILogService logService, string apiKey)
+        {
+            this.logService = logService;
+            api = new MandrillApi(apiKey);
+        }
+
+        public async Task<bool> SendAsync(string from, string fromName, string to, string toName, string subject, string message)
+        {
+            try
+            {
+                var fromAddress = new MandrillMailAddress(from, fromName);
+                var toAddress = new MandrillMailAddress(to, toName);
+                var mandrillMessage = new MandrillMessage(fromAddress, toAddress);
+                var result = await api.Messages.SendAsync(mandrillMessage);
+                if (result.Any(w => w.Status == MandrillSendMessageResponseStatus.Invalid || w.Status == MandrillSendMessageResponseStatus.Rejected))
+                {
+                    logService.Info(result.FirstOrDefault().RejectReason);
+                    return false;
+                }
+                else
+                {
+                    logService.Info(result.FirstOrDefault().RejectReason);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                logService.Error("Mandrill Error", ex);
+                return false;
+            }
+
+        }
+
+        public Task<bool> SendWithTemplateAsync(string from, string fromName, string to, string toName, string subject, string template, Dictionary<string, object> globalVars, Dictionary<string, object> vars)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrimeWeb/PrimeWeb.Util/HttpUtils/HttpClientWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Util.HttpUtils
+{
+    /// <summary>
+    /// Generic HttpClient wrapper
+    /// </summary>
+    public class HttpClientWrapper
+    {
+        /// <summary>
+        /// Recommend the client as static from
+        /// https://stackoverflow.com/questions/4015324/how-to-make-http-post-web-request
+        /// </summary>
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        /// <summary>
+        /// Post encoded form values and then read response
+        /// </summary>
+        /// <param name="url">Post url</param>
+        /// <param name="values">Encoded string key value pair</param>
+        /// <returns>Response as string</returns>
+        public async Task<string> HttpPostAsync(string url, Dictionary<string, string> values)
+        {
+            var content = new FormUrlEncodedContent(values);
+            var response = await httpClient.PostAsync(url, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Get url response data
+        /// </summary>
+        /// <param name="url">Url</param>
+        /// <returns>Response as string</returns>
+        public async Task<string> HttpGetAsync(string url)
+        {
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}

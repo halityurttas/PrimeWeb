869d142fc27b27d837115dac31253cd8020b5010
Halit Yurttas <halityurttas@gmail.com>
2017-10-23 03:36:35 +0300

    Continue utils

---
+++ PrimeWeb/PrimeWeb.Util/Reflection/StringSerialization.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PrimeWeb.Util.Reflection
+{
+    public class StringSerializer
+    {
+        public static string Serialize<T>(T Object) where T : class
+        {
+            XmlSerializer srlz = new XmlSerializer(typeof(T));
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new Utf8StringWriter(sb);
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            srlz.Serialize(sw, Object, ns);
+            return sb.ToString();
+        }
+
+        public static T Deserialize<T>(string Data) where T : class
+        {
+            XmlSerializer srlz = new XmlSerializer(typeof(T));
+            StringReader sr = new StringReader(Data);
+            return srlz.Deserialize(sr) as T;
+        }
+
+
+        private class Utf8StringWriter : StringWriter
+        {
+            public Utf8StringWriter(StringBuilder sb)
+                : base(sb)
+            {
+            }
+            public override Encoding Encoding { get { return Encoding.UTF8; } }
+        }
+
+    }
+}

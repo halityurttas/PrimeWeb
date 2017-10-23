6a194c40961c9f1da0d77da2f6fc268dca88641a
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-10-23 18:40:42 +0300

    Start cdn

---
+++ PrimeWeb/PrimeWeb.Service/CDNService/FileSystemCDNService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Service.CDNService
+{
+    public class FileSystemCDNService: ICDNService
+    {
+        private string path;
+        private string vpath;
+
+        public FileSystemCDNService(string path, string vpath)
+        {
+            this.path = path;
+            this.vpath = vpath;
+        }
+
+        public string GetPath(string relativePath)
+        {
+            return (vpath.EndsWith("/") ? vpath : vpath + "/") + relativePath;
+        }
+
+        public void Save(byte[] content, string filename, string folder)
+        {
+            if ()
+        }
+    }
+}

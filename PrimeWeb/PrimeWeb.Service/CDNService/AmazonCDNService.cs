b6f58489d95dc3162cc31f82c7d610d81a75d5a6
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-10-25 16:36:18 +0300

    AWS CDN continue

---
+++ PrimeWeb/PrimeWeb.Service/CDNService/AmazonCDNService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb.Service.CDNService
+{
+    public class AmazonCDNService : ICDNService
+    {
+        private ILogService logService;
+
+        public string GetPath(string relativePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(byte[] content, string filename, string folder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

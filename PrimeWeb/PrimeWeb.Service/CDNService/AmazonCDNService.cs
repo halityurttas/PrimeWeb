0d118e27f1f1013afb02afcbe87a58a06d6ad427
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-10-27 13:37:07 +0300

    Entity framework update Amazon S3 CDN service complete

--- PrimeWeb/PrimeWeb.Service/CDNService/AmazonCDNService.cs
+++ PrimeWeb/PrimeWeb.Service/CDNService/AmazonCDNService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +13,69 @@ namespace PrimeWeb.Service.CDNService
     public class AmazonCDNService : ICDNService
     {
         private ILogService logService;
+        private string accessKey;
+        private string secretKey;
+        private string bucket;
+
+        public AmazonCDNService(ILogService logService, string accessKey, string secretKey, string bucket)
+        {
+            this.logService = logService;
+            this.accessKey = accessKey;
+            this.secretKey = secretKey;
+            this.bucket = bucket;
+        }
 
         public string GetPath(string relativePath)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var amazonS3 = new AmazonS3Client(accessKey, secretKey);
+                var bucketResponse = amazonS3.ListBuckets();
+                var awsBucket = bucketResponse.Buckets.FirstOrDefault(w => w.BucketName == bucket);
+                if (awsBucket == null)
+                {
+                    return "";
+                }
+                else
+                {
+                    return amazonS3.GetPreSignedURL(new GetPreSignedUrlRequest
+                    {
+                        BucketName = bucket,
+                        Key = relativePath,
+                        Expires = DateTime.MaxValue
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                logService.Error("AmazonS3", ex);
+                return "";
+            }
         }
 
         public void Save(byte[] content, string filename, string folder)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var amazonS3 = new AmazonS3Client(accessKey, secretKey);
+                using (var ms = new MemoryStream(content))
+                {
+                    var transferUtility = new TransferUtility(amazonS3);
+                    var transferUtilityRequest = new TransferUtilityUploadRequest()
+                    {
+                        BucketName = bucket,
+                        StorageClass = S3StorageClass.ReducedRedundancy,
+                        CannedACL = S3CannedACL.PublicRead,
+                        Key = string.IsNullOrEmpty(folder) ? filename : folder.EndsWith("/") ? folder + filename : folder + "/" + filename,
+                        InputStream = ms
+                    };
+                    transferUtility.Upload(transferUtilityRequest);
+                }
+            }
+            catch (Exception ex)
+            {
+                logService.Error("AmazonS3", ex);
+            }
         }
     }
 }

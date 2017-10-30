60a96eb4ea5714becda88fc15d15f07c40bde2ab
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-10-30 11:16:06 +0300

    Auth setting

--- PrimeWeb/PrimeWeb.App.WebForm/Startup.cs
+++ PrimeWeb/PrimeWeb.App.WebForm/Startup.cs
@@ -4,10 +4,12 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(PrimeWeb.App.WebForm.Startup))]
 namespace PrimeWeb.App.WebForm
 {
-    public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+    public partial class Startup : Framework.Config.ApplicationConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            InitializeAuth(app);
             ConfigureAuth(app);
-            
         }
     }
 }

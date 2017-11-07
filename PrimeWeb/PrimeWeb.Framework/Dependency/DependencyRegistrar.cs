7cba63680ba941d83815811322c8f74dd4dd0d4e
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-11-07 09:37:07 +0300

    Remove unity

--- PrimeWeb/PrimeWeb.Framework/Dependency/DependencyRegistrar.cs
+++ PrimeWeb/PrimeWeb.Framework/Dependency/DependencyRegistrar.cs
@@ -1,5 +1,4 @@
-﻿using Unity.Lifetime;
-using PrimeWeb.Service;
+﻿using PrimeWeb.Service;
 using PrimeWeb.Service.LogService;
 using PrimeWeb.Core.Data;
 using PrimeWeb.Repo;
@@ -10,13 +9,6 @@ namespace PrimeWeb.Framework.Dependency
 {
     public static class DependencyRegistrar
     {
-        public static void RegisterBase(Microsoft.Practices.Unity.IUnityContainer container)
-        {
-            Core.Infrastructure.Singleton.Add(container);
-            container.RegisterType<IUnitOfWork<DBContext>, UnitOfWork>();
-            container.RegisterType<ILogService, Log4NetService>(new ContainerControlledLifetimeManager());
-            container.RegisterType<IUserRepository, UserRepository>();
-            container.RegisterType<IRoleRepository, RoleRepository>();
-        }
+        
     }
 }

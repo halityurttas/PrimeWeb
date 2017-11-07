a96684b9f862c5c31c4f2ae520ff1bb7b72afdb2
Halit Yurttaş <halit.yurttas@kumsaati.com.tr>
2017-11-07 10:39:55 +0300

    Ninject continue

--- PrimeWeb/PrimeWeb.Framework/Dependency/DependencyRegistrar.cs
+++ PrimeWeb/PrimeWeb.Framework/Dependency/DependencyRegistrar.cs
@@ -4,11 +4,20 @@ using PrimeWeb.Core.Data;
 using PrimeWeb.Repo;
 using PrimeWeb.Data;
 using PrimeWeb.Repo.Implementation;
+using Ninject;
+using Ninject.Web.Common;
+using PrimeWeb.Service.CacheService;
 
 namespace PrimeWeb.Framework.Dependency
 {
     public static class DependencyRegistrar
     {
-        
+        public static void RegisterCoreModules(IKernel kernel)
+        {
+            kernel.Bind<IUnitOfWork<DBContext>>().To<UnitOfWork>().InRequestScope().WithConstructorArgument(DBContext.Create());
+            kernel.Bind<IUserRepository>().To<UserRepository>().InRequestScope();
+            kernel.Bind<IRoleRepository>().To<RoleRepository>().InRequestScope();
+            kernel.Bind<ILogService>().To<Log4NetService>().InSingletonScope();
+        }
     }
 }
